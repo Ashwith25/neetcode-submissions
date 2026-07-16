6a9278ba16bbccd59fcffb58d7f1c340284f4caf
Ashwith Ananda Poojary <pashwith25@gmail.com>
2026-07-16 14:10:49 -0700

    Add: car-fleet - submission-13

---
+++ Data Structures & Algorithms/car-fleet/submission-13.py
@@ -0,0 +1,24 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        map = {}
+        for idx, i in enumerate(position):
+            map[i] = speed[idx]
+
+        position.sort()
+        position = position[::-1]
+
+        times = [(target - i) / map[i] for i in position]
+
+        stack = [times[0]]
+        # print(times, position)
+        i=0
+        while i<len(position):
+            # stack.append(times[i])
+            if stack and stack[-1]<times[i]:
+                stack.append(times[i])
+                # stack.pop()
+            
+            # print(stack)
+            i+=1
+
+        return len(stack)
