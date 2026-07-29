b1b601c01bcb1a75d753dadd36f13bd39e69afd4
Ashwith Ananda Poojary <pashwith25@gmail.com>
2026-07-28 23:21:24 -0700

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += f"{len(s)}#{s}"
+
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        i=0
+        res = []
+        print(s)
+        while i<len(s):
+            if s[i].isnumeric():
+                j=i
+                while s[i].isnumeric():
+                    i+=1
+            # print(s[j:i], i, j)
+            number = int(s[j:i])
+            i+=1
+
+            res.append(s[i:number+i])
+            # print(res)
+            i+=number
+
+        return res
