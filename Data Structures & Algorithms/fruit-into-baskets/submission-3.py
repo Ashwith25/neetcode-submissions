928073b5b7b30655d0ff370ae242ffee63387b5d
Ashwith Ananda Poojary <pashwith25@gmail.com>
2026-07-28 22:57:07 -0700

    Add: fruit-into-baskets - submission-3

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        hashset = {}
+
+        i = 0
+        j=0
+        res = 0
+
+        while j <= len(fruits):
+            res = max(res, j-i)
+            if j==len(fruits):
+                return max(res, j-i)
+            hashset[fruits[j]] = hashset.get(fruits[j], 0) + 1
+            while len(hashset.keys())>2 and i<j:
+                hashset[fruits[i]] -= 1
+                if hashset[fruits[i]]==0:
+                    del hashset[fruits[i]]
+                i+=1
+            j+=1
+
+        return res
+                    
